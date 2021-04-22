41e6c3187b09d628fb15762efceb0f8c4ea6535f
guillerminafrenguelli <81364317+guillerminafrenguelli@users.noreply.github.com>
2021-04-22 09:00:08 -0300

    Add files via upload

---
+++ ejercicio16.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc07
+ */
+public class ejercicio16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num=1;
+        
+        for( int i=0; i<=100; i++){
+            if  (num%2==0 || num%3==0){
+                System.out.println(num);
+        }
+        num++;
+    }
+ }   
+}
